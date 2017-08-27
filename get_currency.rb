af4983e31450cd7ee698211ce9745b08622005a8
Qew7 <qewwork@rambler.ru>
2017-08-27 22:53:56 +0300

    rubocoped

--- get_currency.rb
+++ get_currency.rb
@@ -1,33 +1,30 @@
 require 'open-uri'
 require 'nokogiri'
 
-class Exchange_Rate
+class ExchangeRate
+  VAL = 'R01235'.freeze # код валюты для апи банка
 
   def initialize(date)
-
-    @val = 'R01235' #код валюты для апи банка
-
     @date = date.nil? ? Time.now : Time.parse(date)
-
   end
 
-  def date_incorrect? #проверка на выходные банка
-    (@date.monday? or @date.sunday?) ? true : false
+  def date_incorrect? # проверка на выходные банка
+    @date.monday? || @date.sunday? ? true : false
   end
 
-  def correct_date #исправление на день или два если это выходной банка
-    @date = @date.wday == 0 ? @date - ( 60 * 60 * 24 ) : @date - ( 60 * 60 * 48 )
-  end 
+  def correct_date # исправление на день или два если это выходной банка
+    @date = @date.wday.zero? ? @date - (60 * 60 * 24) : @date - (60 * 60 * 48)
+  end
 
-  def get_url
-    "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=#{@date}&date_req2=#{@date}&VAL_NM_RQ=#{@val}"
+  def url
+    "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=#{@date}&date_req2=#{@date}&VAL_NM_RQ=#{VAL}"
   end
 
   def get_rate
-    @rate = Nokogiri::HTML(open(get_url)).xpath('//value').text
+    @rate = Nokogiri::HTML(open(url)).xpath('//value').text
   end
 
-  def save_rate #писать решил в файл потому что бд для одного значения делать неадекватно
+  def save_rate # писать решил в файл потому что бд для одного значения делать неадекватно
     File.write('Последний курс', @rate)
   end
 
@@ -36,47 +33,34 @@ class Exchange_Rate
   end
 
   def rate
+    return load_rate if @date > Time.now
 
-    if (@date > Time.now)
-      return load_rate
-    end
+    correct_date if date_incorrect?
 
-    if (date_incorrect?)
-      correct_date
-    end
+    @date = @date.strftime('%d/%m/%Y')
 
-    @date = @date.strftime("%d/%m/%Y")  
-
-    3.times do |i| #3 раза стучимся, если необходимое значение не получено, возвращаем последнее сохранённое значение
-      
+    3.times do |_i| # 3 раза стучимся, если необходимое значение не получено, возвращаем последнее сохранённое значение
       begin
-        if (! get_rate.empty?)
-          break
-        end
+        break unless get_rate.empty?
       rescue OpenURI::HTTPError
         next
       end
-
     end
 
-    if(! @rate.nil?)
+    if !@rate.nil?
       save_rate
-      return @rate
-    else 
-      return load_rate
-    end  
-
+      @rate
+    else
+      load_rate
+    end
   end
-
 end
 
 begin
-  currency = Exchange_Rate.new(ARGV[0])
+  currency = ExchangeRate.new(ARGV[0])
   p currency.rate
 rescue ArgumentError
   p 'Формат даты не верен, последний курс доллара:'
-  #гружу данные не методом потому что класс не инициализировался
+  # гружу данные не методом потому что класс не инициализировался
   p File.exist?('Последний курс') ? File.read('Последний курс') : 'Нет сохранённого курса'
 end
-
-
